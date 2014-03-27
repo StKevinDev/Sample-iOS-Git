afaa30c79861899a9ce4f3b46e124e88213a5c48
Kevin <kevin@Smarts-Mac-mini.local>
2014-03-27 15:30:43 +0800

    second commit

---
+++ Git test/STTest.h
@@ -0,0 +1,13 @@
+//
+//  STTest.h
+//  Git test
+//
+//  Created by Kevin on 3/27/14.
+//  Copyright (c) 2014 Smart Traffic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface STTest : NSObject
+
+@end
---
+++ Git test/STTest.m
@@ -0,0 +1,13 @@
+//
+//  STTest.m
+//  Git test
+//
+//  Created by Kevin on 3/27/14.
+//  Copyright (c) 2014 Smart Traffic. All rights reserved.
+//
+
+#import "STTest.h"
+
+@implementation STTest
+
+@end
